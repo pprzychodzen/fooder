3b00802ffbe08581c7cd0e3a629187d4e1a648d0
Paweł Przychodzeń <przychu@gmail.com>
2020-02-04 15:29:34 +0100

    pl in register & login form

--- main/views.py
+++ main/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from main.models import Category
 
 
 # Create your views here.
 def homepage(request):
     return render(request, 'main/home.html')
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = "main/category.html"
+    queryset = Category.objects.all()
+
--- user/forms.py
+++ user/forms.py
@@ -1,18 +1,37 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from user.models import UserProfile
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='nie wymagane')
-    last_name = forms.CharField(max_length=30, required=False, help_text='nie wymagane')
-    email = forms.EmailField(max_length=254, required=True, help_text="Proszę podać prawidłowy email!")
+    first_name = forms.CharField(max_length=30, required=False, help_text='niewymagane', label='Imię')
+    last_name = forms.CharField(max_length=30, required=False, help_text='niewymagane', label='Nazwisko')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address')
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
+        for fieldname in ['password1']:
+            self.fields[fieldname].label = 'Hasło'
+
+        for fieldname in ['password2']:
+            self.fields[fieldname].label = 'Powtórz hasło'
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
-        help_texts = {
-            'username': None,
-            'password1': None,
-            'password2': None
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
+        labels = {
+            'username': 'Nazwa użytkownika',
+            'password1': 'hasło',
+            'password2': 'powtórz hasło'
         }
+
+# class UserProfileForm(forms.ModelForm):
+#     class Meta:
+#         model = UserProfile
+#         # fields = ('avatar',)
+#         # labels = {'avatar': 'Awatar'}
