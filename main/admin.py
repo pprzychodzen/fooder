7e0a1fe4989361cbbc10b84bb9177aaa9514fa01
Paweł Przychodzeń <przychu@gmail.com>
2020-02-12 15:30:36 +0100

    added in Models: tag preparation field
    whole implemented in templates (home and detail)

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import Category, Recipe
-
+from .models import Category, Recipe, Tag
 
 # Register your models here.
 
 
-
 admin.site.register(Category)
 admin.site.register(Recipe)
+admin.site.register(Tag)
--- main/models.py
+++ main/models.py
@@ -10,22 +10,20 @@ class Category(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
 class Recipe(models.Model):
-    DINNER = 'Danie główne'
-    SUPPER = 'Kolacja'
-    DESSERT = 'Deser'
-    STARTER = 'Przystawka'
-    category_choices = [
-        (DINNER, 'Danie główne'),
-        (SUPPER, 'Kolacja'),
-        (DESSERT, 'Deser'),
-        (STARTER, 'Przystawka'),
-
-    ]
-
     title = models.CharField(max_length=200)
     description = tinymce_models.HTMLField()
-    recipe_category = models.CharField(max_length=30, choices=category_choices, default=DINNER)
+    preparing = models.TextField(default="pusty")
+    recipe_category = models.ForeignKey(Category, default=None, blank=False, on_delete=models.CASCADE,
+                                        related_name='category')
+    tag = models.ManyToManyField(Tag, blank=True, related_name='recipe_tags')
 
     def get_absolute_url(self):
         return reverse('recipe:detail', kwargs={'id': self.id})
