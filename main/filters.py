41d3be832b8c8189b0c5b67349024ed8dfd6ac0e
Paweł Przychodzeń <przychu@gmail.com>
2020-02-13 10:51:59 +0100

    search bar works

---
+++ main/filters.py
@@ -0,0 +1,13 @@
+import django_filters
+from django.forms.widgets import TextInput
+
+from main.models import Recipe
+
+
+class RecipeFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(label="", lookup_expr='icontains',
+                                      widget=TextInput(attrs={'placeholder': 'szukaj przepisu'}))
+
+    class Meta:
+        model = Recipe
+        fields = ['title']
